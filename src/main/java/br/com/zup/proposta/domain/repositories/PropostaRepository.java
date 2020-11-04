2b937f380eaf6325320e114de1a3c241949b4174
wildrimak <wildrimak@gmail.com>
2020-11-04 01:08:50 -0300

    010: atualizado para botar findByDocumento

--- src/main/java/br/com/zup/proposta/domain/repositories/PropostaRepository.java
+++ src/main/java/br/com/zup/proposta/domain/repositories/PropostaRepository.java
@@ -1,9 +1,13 @@
 package br.com.zup.proposta.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.zup.proposta.domain.models.Proposta;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
 
+	public Optional<Proposta> findByDocumento(String documento);
+	
 }
