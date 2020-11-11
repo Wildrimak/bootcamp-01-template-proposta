f78077b90a234c97d5ca5fc8f72e6dcb8a465c05
wildrimak <wildrimak@gmail.com>
2020-11-11 00:32:33 -0300

    035: Acompanhamento da proposta concluido!

---
+++ src/main/java/br/com/zup/proposta/api/controllers/AcompanharPropostaController.java
@@ -0,0 +1,35 @@
+package br.com.zup.proposta.api.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.zup.proposta.domain.models.Proposta;
+import br.com.zup.proposta.domain.repositories.PropostaRepository;
+
+@RestController
+@RequestMapping("/propostas")
+public class AcompanharPropostaController {
+
+	@Autowired
+	private PropostaRepository propostaRepository; // 1
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> detalheCompra(@PathVariable Long id) {
+
+		Optional<Proposta> optionalProposta = propostaRepository.findById(id);
+
+		// 1
+		if (optionalProposta.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		return ResponseEntity.ok(optionalProposta.get());
+	}
+
+}
